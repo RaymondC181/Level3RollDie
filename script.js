9468b3ebc8e623d095bea83df2e800eb016f8cf4
RaymondC181 <99200228+RaymondC181@users.noreply.github.com>
2022-12-09 13:52:22 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+dieOne = false; 
+dieTwo = false; 
+dieThree = false; 
+
+
+firstDie = 1; 
+secondDie = 1; 
+thirdDie = 1; 
+
 function initialize()
 {
     numRolls = document.getElementById("num"); 
-
     rollingNum = 0; 
+
+    dieOne = true; 
+    dieTwo = false; 
+    dieThree = false; 
     
 }
 
 function add()
 {
-    rollingNum+=1; 
-    display(); 
+    if(rollingNum<6)
+   {
+    rollingNum++; 
+   }
+   display();
 }
 
 function display()
@@ -17,6 +32,73 @@ function display()
     numRolls.innerHTML = rollingNum; 
 }
 
+function minus()
+{
+   if(rollingNum>1)
+   {
+    rollingNum--; 
+   }
+   display();
+}
 
+function oneDie()
+{
+    dieOne = true; 
+    dieTwo = false; 
+    dieThree = false; 
+
+    console.clear();
+    console.log("Die one: " + dieOne);
+    console.log("Die two: " + dieTwo);
+    console.log("Die three: " + dieThree);
+}
+function twoDie()
+{
+    dieOne = false; 
+    dieTwo = true; 
+    dieThree = false; 
+
+    console.clear();
+    console.log("Die one: " + dieOne);
+    console.log("Die two: " + dieTwo);
+    console.log("Die three: " + dieThree);
+}
+function threeDie()
+{
+    dieOne = false; 
+    dieTwo = false; 
+    dieThree = true; 
+
+    console.clear();
+    console.log("Die one: " + dieOne);
+    console.log("Die two: " + dieTwo);
+    console.log("Die three: " + dieThree);
+}
+
+function roll()
+{
+    amountOfRolls = (document.getElementById("num")); 
+    if(dieOne)
+    {
+        firstDie = parseInt((Math.random() * (6 - 1 + 1) + 1))
+    }
+    if(dieTwo)
+    {
+        firstDie = parseInt((Math.random() * (6 - 1 + 1) + 1))
+        secondDie = parseInt((Math.random() * (6 - 1 + 1) + 1))
+    }
+    if(dieThree)
+    {
+        firstDie = parseInt((Math.random() * (6 - 1 + 1) + 1))
+        secondDie = parseInt((Math.random() * (6 - 1 + 1) + 1))
+        thirdDie = parseInt((Math.random() * (6 - 1 + 1) + 1))
+    }
+
+    console.clear();
+    console.log(firstDie);
+    console.log(secondDie);
+    console.log(thirdDie);
+
+}
 
 
